36533274a0f143617fe1c7a12278e9bbde99e4be
mbalaji2510 <mbalaji2510@gmail.com>
2020-08-17 09:56:26 +0530

    first commit

---
+++ spring-boot-data-mongodb/src/main/java/com/infosys/springbootdatamongodb/SpringBootDataMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.infosys.springbootdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataMongodbApplication.class, args);
+		System.out.println("Welcome Kishore!!!");
+	}
+
+}
---
+++ spring-boot-data-mongodb/src/main/java/com/infosys/springbootdatamongodb/model/Employee.java
@@ -0,0 +1,56 @@
+package com.infosys.springbootdatamongodb.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection= "employee")
+public class Employee {
+ 
+	@Id
+    private String id;
+    private String name;
+    private String designation;
+ 
+    public Employee() { }    
+    
+    
+
+
+	public Employee(String name, String designation) {
+		super();
+		this.name = name;
+		this.designation = designation;
+	}
+
+
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+
+
+	public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDesignation() {
+        return designation;
+    }
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", designation=" + designation + "]";
+    }
+}
